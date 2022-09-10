b8789bffb8cee47a65abdef4b52d79fc943a8d24
Benjamin Rabier <rabier.benj@gmail.com>
2022-09-10 14:51:19 +0200

    Add Flake.nix

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Rust GraphQL Exercise";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+      let pkgs = nixpkgs.legacyPackages.${system}; in
+      with pkgs;
+        {
+          devShells.default = mkShell {
+            # Unfortunately retrieving Rust through Flake didn't work with the intellij plugin. Only with VScdoe
+            nativeBuildInputs = [ 
+              terraform
+            ];  
+          };
+        }
+      );
+}
+
